05aab2e0a79006fd3c76c464e453ac1caa1b249b
frankcw <frankcordovaw@gmail.com>
2015-08-21 03:09:10 -0400

    newDirectory

---
+++ CBT/03.py
@@ -0,0 +1,11 @@
+userName = "fcordova"
+fullName = "Frank Cordova"
+
+print(userName)
+print(fullName)
+
+altName = userName
+incorrectName = 'fullName'
+newName = userName = fullName
+print(newName)
+print(userName)
