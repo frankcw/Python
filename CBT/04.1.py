a63db6fc54046eaebc3ecc16ac6848883c77a4ab
frankcw <frankcordovaw@gmail.com>
2015-08-22 03:18:11 -0400

    Module4

---
+++ CBT/04.1.py
@@ -0,0 +1,13 @@
+#Input and Outputs
+#Operators =,+,-,*,/
+#print function (sep = " ", end ="/n")
+
+print('Hello World!', end = "")
+print('Hello', 'World', '!', sep="")
+print('This is', 'an example')
+
+
+
+
+
+
---
+++ CBT/04.2.py
@@ -0,0 +1,9 @@
+#input and output
+#Use of operators for print
+
+userName = input('Please enter your name:')
+
+finalName = userName+'!'
+
+print('Hello', finalName)
+
