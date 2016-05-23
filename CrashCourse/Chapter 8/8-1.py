3eb51b1a3a9c681808cecd94738e7fca5f9ac517
frankcw <frankcordovaw@gmail.com>
2016-05-22 23:19:36 -0400

    new chapter

---
+++ CrashCourse/Chapter 8/8-1.py
@@ -0,0 +1,8 @@
+#simple functions
+
+def display_message():
+	"""displaying messages"""
+	print('I am learning about functions.')
+
+display_message()
+
---
+++ CrashCourse/Chapter 8/8-2.py
@@ -0,0 +1,7 @@
+#functions with parameters
+
+def favorite_book(book):
+	"""arugments in functions"""
+	print('My favorite book is ' + book + '.')
+
+favorite_book('Alice in Wonderland')
