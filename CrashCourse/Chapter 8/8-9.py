dde60c5c97cac7dc6a488d61e28b1fb261b16678
Frank_Mac <frankcordovaw@gmail.com>
2016-07-24 13:34:41 -0400

    new labs

--- CrashCourse/Chapter 8/8-9.py
+++ CrashCourse/Chapter 8/8-9.py
@@ -1,6 +1,10 @@
 #function with a list
 
-magician_names = ['magicarp','mr. mime'.'abra']
+magician_names = ['magicarp','mr. mime','abra']
 
 def show_magicians(magician):
-		
+	for magician in magician_names:
+		print(magician.title())
+
+name = show_magicians
+name(magician_names)
